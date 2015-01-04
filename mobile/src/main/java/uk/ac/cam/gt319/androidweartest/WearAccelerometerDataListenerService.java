5f7c2273f9de756b297acc552611a73919d84c74
George Thomas <georgetamayothomas@gmail.com>
2015-01-04 23:35:41 +0000

    DataListenerService now writes bytes to file

--- mobile/src/main/java/uk/ac/cam/gt319/androidweartest/WearAccelerometerDataListenerService.java
+++ mobile/src/main/java/uk/ac/cam/gt319/androidweartest/WearAccelerometerDataListenerService.java
@@ -1,5 +1,7 @@
 package uk.ac.cam.gt319.androidweartest;
 
+import android.os.Environment;
+import android.text.format.Time;
 import android.util.Log;
 
 import com.google.android.gms.wearable.DataEvent;
@@ -7,7 +9,10 @@ import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.WearableListenerService;
 
-import uk.ac.cam.gt319.accelerometerdata.AccelerometerDataBlob;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by George on 15/12/14.
@@ -21,7 +26,36 @@ public class WearAccelerometerDataListenerService extends WearableListenerServic
     Log.d(TAG, "onDataChanged called");
     for (DataEvent d : dataEvents) {
       DataMap dm = DataMap.fromByteArray(d.getDataItem().getData());
-      AccelerometerDataBlob dataBlob = new AccelerometerDataBlob(dm.getByteArray("AccelDataFromWear"));
+      saveToDisk(dm.getByteArray("AccelDataFromWear"));
     }
+  }
+
+  private void saveToDisk(byte[] dataBlobArray) {
+    Time now = new Time();
+    now.setToNow();
+    String filename = "20150104" +  ".dat";
+    File dir = getStorageDir("accelData");
+    dir.mkdirs();
+    File file = new File(dir.getPath() + filename);
+    Log.d(TAG, dir.getAbsolutePath());
+    try {
+      file.createNewFile();
+      FileOutputStream f = new FileOutputStream(file);
+      f.write(dataBlobArray);
+      f.close();
+    } catch (FileNotFoundException e) {
+      Log.wtf(TAG, "Saving to disk - file not found", e);
+    } catch (IOException e) {
+      Log.wtf(TAG, "Saving to disk - IOException", e);
+    }
+  }
+
+  private File getStorageDir(String albumName) {
+    File file = new File(Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_DOCUMENTS), albumName);
+    if (!file.mkdirs()) {
+      Log.e(TAG, "Directory not created");
+    }
+    return file;
   }
 }
