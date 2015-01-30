e7a1e56a06de437b2aea2946a1afddf4416b5975
George Thomas <georgetamayothomas@gmail.com>
2015-01-30 17:46:35 +0000

    Use the FileSaver in WearDataListener

--- mobile/src/main/java/uk/ac/cam/gt319/androidweartest/WearAccelerometerDataListenerService.java
+++ mobile/src/main/java/uk/ac/cam/gt319/androidweartest/WearAccelerometerDataListenerService.java
@@ -1,36 +1,22 @@
 package uk.ac.cam.gt319.androidweartest;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.SystemClock;
-import android.text.format.Time;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.DataMapItem;
-import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import uk.ac.cam.gt319.accelerometerdata.FileSaver;
 
 /**
  * Created by George on 15/12/14.
@@ -75,10 +61,6 @@ public class WearAccelerometerDataListenerService extends WearableListenerServic
   }
 
   private void saveToDisk(Asset dataBlobAsset) {
-    File dir = getStorageDir("accelData");
-    String filename = genFilename("");
-    File file = new File(dir.getPath() + filename);
-    Log.d(TAG, dir.getAbsolutePath());
     ConnectionResult result = googleApiClient.blockingConnect(5000, TimeUnit.MILLISECONDS);
     if (!result.isSuccess()) {
       Log.d(TAG, "Unsuccessful connection");
@@ -89,34 +71,8 @@ public class WearAccelerometerDataListenerService extends WearableListenerServic
         googleApiClient, dataBlobAsset).await().getInputStream();
     googleApiClient.disconnect();
 
-
-    if (assetInputStream == null) {
-      Log.w(TAG, "Requested an unknown Asset.");
-      return;
-    }
-
-    try {
-      file.createNewFile();
-      FileOutputStream fileOutputStream = new FileOutputStream(file);
-      byte[] buf = new byte[1024];
-      int len;
-      while ((len = assetInputStream.read(buf)) != -1) {
-        fileOutputStream.write(buf, 0, len);
-      }
-      Log.d(TAG, "Saved to " + file.getAbsolutePath());
-      fileOutputStream.close();
-    } catch (FileNotFoundException e) {
-      Log.wtf(TAG, "Saving to disk - file not found", e);
-    } catch (IOException e) {
-      Log.wtf(TAG, "Saving to disk - IOException", e);
-    }
-  }
-
-  private File getStorageDir(String albumName) {
-    File file = new File(Environment.getExternalStoragePublicDirectory(
-        Environment.DIRECTORY_DOCUMENTS), albumName);
-    file.mkdirs();
-    return file;
+    FileSaver fileSaver = new FileSaver(getFileName());
+    fileSaver.saveToDisk(assetInputStream);
   }
 
   private GoogleApiClient buildGoogleApiClient() {
@@ -142,14 +98,8 @@ public class WearAccelerometerDataListenerService extends WearableListenerServic
         .build();
   }
 
-  private String genFilename(String prefix) {
-    DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHms");
-    String filename = "/" + getFileName() + "-" + dateFormat.format(new Date()) + ".dat";
-    return filename;
-  }
-
   public String getFileName() {
-    return fileName;
+    return "wear-" + fileName;
   }
 
   public void setFileName(String fileName) {
