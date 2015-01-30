ebcfb4297bb03ccc4ed52ef02896fbce96fb68f8
George Thomas <georgetamayothomas@gmail.com>
2015-01-30 16:35:49 +0000

    Create a PhoneAccelerometerDataCaptureService

---
+++ mobile/src/main/java/uk/ac/cam/gt319/androidweartest/PhoneAcceleometerDataCaptureService.java
@@ -0,0 +1,14 @@
+package uk.ac.cam.gt319.androidweartest;
+
+import uk.ac.cam.gt319.accelerometerdata.AccelerometerDataCaptureService;
+
+/**
+ * Created by George on 30/01/15.
+ */
+public class PhoneAcceleometerDataCaptureService extends AccelerometerDataCaptureService {
+
+  @Override
+  public void onDestroy() {
+    
+  }
+}
